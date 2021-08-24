2423615b8ddcfe167e02ba24dc18a843fc641aa5
Peter Jiang <47200772+CheatCod@users.noreply.github.com>
2021-08-23 21:11:35 -0400

    server fixed
    Co-authored-by: Kevin Huang <Ynng@users.noreply.github.com>

--- Server.js
+++ Server.js
@@ -1,60 +1,30 @@
-var express = require('express');
-const { StringDecoder } = require('string_decoder');
-const decoder = new StringDecoder('utf8');
-const { startServer, createConfiguration, build } = require('snowpack');
-var app = express();
-const port = 3000;
-
-// import c from './snowpack.config.mjs';
-const config = createConfiguration({
-  mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
-  },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
-});
-let snowpackServer;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+app.use(bodyParser.json());
 
 function endsWith(str, subStr) {
   return str.indexOf(subStr) === str.length - subStr.length;
 }
 
-app.get('*', async (req, res, next) => {
-  console.log('got request', req.url);
-  try {
-    const buildResult = await snowpackServer.loadUrl(req.url);
-    if (endsWith(req.url, '.js')) {
-      res.contentType('application/javascript');
-    }
-    res.send(decoder.write(buildResult.contents));
-  } catch (e) {
-    next(e);
+app.use((req, res, next) => {
+  res.set('Cross-Origin-Embedder-Policy', 'require-corp');
+  res.set('Cross-Origin-Opener-Policy', 'same-origin');
+
+  if (endsWith(req.url, '.js')) {
+    res.contentType('application/javascript');
   }
+  next();
+  // res.sendFile(path.join(__dirname, 'build/'));
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+// app.use(cors());
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log('Listening on port', port);
 });
-
-async function startSnowpackServer() {
-  snowpackServer = await startServer({ config });
-}
-
-startSnowpackServer();
