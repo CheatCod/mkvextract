f34ff46a2a72ffbc56824d6cd1b6bf6b83874be9
Peter Jiang <47200772+CheatCod@users.noreply.github.com>
2021-08-27 00:26:06 -0400

    reading subtitle information
    Co-authored-by: Kevin Huang <Ynng@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,47 @@ import './App.css';
 
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
+var curStreamIdx = -1;
+var videoInfo = [];
+
 const ffmpeg = createFFmpeg({
-  log: true,
+  //log: true,
   corePath: 'https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js',
+  logger: ({ type, message }) => {
+    if (type !== 'fferr') return;
+    var curLine = message.trim();
+
+    if (curLine.indexOf('Stream #0:') === 0) {
+      console.log(`Stuff found`, curLine);
+
+      curLine = curLine.slice('Stream #0:'.length);
+      // "4123(eng): Subtitle: subrip"
+      var idLanguage = curLine.slice(0, curLine.indexOf(':'));
+      // "4123(eng)"
+      curStreamIdx = idLanguage.match(/\d+/)[0] - 0;
+
+      var format = '';
+      if (curLine.includes('ass')) format = 'ass';
+      else if (curLine.includes('subrip')) format = 'srt';
+      //TODO: add more file formats
+
+      if (format) {
+        videoInfo[curStreamIdx] = { format };
+        console.log(`Subtitle found`, videoInfo);
+      }
+    }
+
+    if (curLine.indexOf('title') === 0) {
+      var title = curLine.split(':')[1].trim().replace(/['"]+/g, '');
+      if (title) {
+        videoInfo[curStreamIdx] = { ...videoInfo[curStreamIdx], title };
+        console.log(`Subtitle found`, videoInfo);
+      }
+    }
+
+    console.log(curLine);
+  },
+  progress: (p) => console.log(p),
 });
 
 function App() {
@@ -29,14 +67,20 @@ function App() {
     console.log('executing extractSub');
     ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
 
-    await ffmpeg.run(
-      '-benchmark',
-      '-i',
-      'test.mp4',
-      '-map',
-      '0:s:0',
-      'sub.srt',
-    );
+    await ffmpeg.run('-i', 'test.mp4');
+
+    // await ffmpeg.run(
+    //   '-v',
+    //   'error',
+    //   '-stats',
+    //   '-i',
+    //   'test.mp4',
+    //   '-vn',
+    //   '-an',
+    //   '-map',
+    //   `0:${curStreamIdx}`,
+    //   'sub.srt',
+    // );
 
     const data = ffmpeg.FS('readFile', 'sub.srt');
     url = URL.createObjectURL(new Blob([data.buffer], { type: 'srt' }));
